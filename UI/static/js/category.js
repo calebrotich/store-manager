909bf850b3a7217df43a0e7d4aa42a1a23a78a28
Caleb Rotich <calebrotich10@gmail.com>
2018-11-10 18:40:09 +0300

    [Feature #161859836] Consume GET /category endoint
    Consumed from https://store-manager-api-v2.herokuapp.com/api/v2/category

--- UI/static/js/category.js
+++ UI/static/js/category.js
@@ -1,7 +1,8 @@
-// Power categories
+// Consume the categories endpoint from https://store-manager-api-v2.herokuapp.com/api/v2/category
 const categoryAPIURI = "https://store-manager-api-v2.herokuapp.com/api/v2/category";
 const categoryButton = document.querySelector("#submit-category");
 const CategoryMessageBox = document.querySelector("#category-message");
+const categoriesSelect = document.querySelector("#categories-select");
 let userToken = localStorage.userToken;
 
 categoryButton.addEventListener('click', (event) => {
@@ -40,6 +41,7 @@ categoryButton.addEventListener('click', (event) => {
                 CategoryMessageBox.innerHTML = `${message}. You can now create another category.`;
                 categoryButton.value = "Add another category";
                 categoryButton.disabled = false;
+                window.location.replace("manage-product.html");
             }
             else {
                 CategoryMessageBox.innerHTML = message;
@@ -58,3 +60,53 @@ categoryButton.addEventListener('click', (event) => {
         categoryButton.value = "Add category";
     }
 });
+
+function fetchCategories() {
+    // Send GET request to category endpoint
+    CategoryMessageBox.innerHTML = "";
+    fetch(categoryAPIURI, {
+        method: 'GET',
+        headers: {
+            'Authorization': userToken
+        }
+    })
+    .then((response)=> {
+        if(response.status < 500){
+            return response.json()
+        }
+        else{
+            CategoryMessageBox.innerHTML = response;
+        }
+    })
+    .then(function(json_response) {
+        let message = json_response.message;
+        if(message === "Categories fetched successfully") {
+            let categories = json_response.categories;
+            categories.forEach(category => {
+                let option = document.createElement("option");
+                option.innerHTML = category.category_name;
+                option.setAttribute("value", `${category.category_id}`);
+                categoriesSelect.appendChild(option);
+            });
+        }
+        else if (message === "No categories created yet") {
+            let option = document.createElement("option");
+            option.innerHTML = message;
+            option.setAttribute("value", -1);
+            categoriesSelect.appendChild(option);
+        }
+        else if (json_response.Message === "You need to login" ||
+                 json_response.Message === "The token is either expired or wrong") {
+            window.location.replace("../../index.html");
+        }
+        else {
+            CategoryMessageBox.innerHTML = message;
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+        CategoryMessageBox.innerHTML = err;
+    });
+}
+
+fetchCategories();
